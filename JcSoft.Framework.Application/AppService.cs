cdca7aaa0707a60d6ee3865834f2addbebe697e4
jiechen <ijiechen@sina.com>
2016-12-17 08:50:30 +0800

    sqlce ,framework 4.5 运行正常

---
+++ JcSoft.Framework.Application/AppService.cs
@@ -0,0 +1,32 @@
+﻿using JcSoft.Framework.Application.Interfaces.Common;
+using JcSoft.Framework.Data.Context.Interfaces;
+using JcSoft.Framework.Domain.Validation;
+using Microsoft.Practices.ServiceLocation;
+
+namespace JcSoft.Framework.Application
+{
+    public class AppService<TContext> : ITransactionAppService<TContext>
+        where TContext : IDbContext, new()
+    {
+        private IUnitOfWork<TContext> _uow;
+
+        public AppService()
+        {
+            ValidationResult = new ValidationResult();
+        }
+
+        protected ValidationResult ValidationResult { get; private set; }
+
+        public virtual void BeginTransaction()
+        {
+            _uow = ServiceLocator.Current.GetInstance<IUnitOfWork<TContext>>();
+            _uow.BeginTransaction();
+        }
+
+        public virtual void Commit()
+        {
+            _uow.SaveChanges();
+        }
+    }
+
+}
